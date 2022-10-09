28aa6455ea7483224707b95e8a52d9250aee8537
rayhaanbhikha <rayhaanbhikha3@hotmail.com>
2022-10-09 18:58:38 +0100

    extract struct method added.

--- utils.go
+++ utils.go
@@ -1,6 +1,8 @@
 package csvparser
 
-import "reflect"
+import (
+	"reflect"
+)
 
 func isKind(t reflect.Type, k2 reflect.Kind) bool {
 	switch k1 := t.Kind(); k1 {
@@ -10,5 +12,17 @@ func isKind(t reflect.Type, k2 reflect.Kind) bool {
 		return isKind(t.Elem(), k2)
 	default:
 		return false
+	}
+}
+
+func extractStruct(val reflect.Value) (reflect.Value, error) {
+	switch k1 := val.Kind(); k1 {
+	case reflect.Struct:
+		return val, nil
+	case reflect.Pointer:
+		v1 := val.Elem()
+		return extractStruct(v1)
+	default:
+		return reflect.Value{}, ErrCSVRowMustBeAStruct
 	}
 }
